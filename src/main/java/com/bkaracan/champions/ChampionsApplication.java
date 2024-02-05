43e11af2f6e573df753b6187d928b7a1e2a7e439
= <=>
2024-02-05 15:02:55 +0300

    ++v1.0 - Role API completed by all layers

--- src/main/java/com/bkaracan/champions/ChampionsApplication.java
+++ src/main/java/com/bkaracan/champions/ChampionsApplication.java
@@ -1,13 +1,37 @@
 package com.bkaracan.champions;
 
+import com.bkaracan.champions.dto.contact.ContactInfoDTO;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {ContactInfoDTO.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Champions microservice REST API doc.",
+                version = "v1",
+                contact = @Contact(
+                        name = "Burak Karacan",
+                        email = "burakkaracan94@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Champions microservice REST API doc."
+        )
+)
 public class ChampionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChampionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionsApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/bkaracan/champions/service/RoleService.java
@@ -0,0 +1,19 @@
+package com.bkaracan.champions.service;
+
+import com.bkaracan.champions.bean.role.ListRoleBean;
+import com.bkaracan.champions.dto.RoleDTO;
+import com.bkaracan.champions.responsepayload.ResponsePayload;
+
+import java.util.List;
+
+public interface RoleService {
+
+    ResponsePayload<RoleDTO> findRoleById(Long roleId);
+
+    ResponsePayload<RoleDTO> saveRole(RoleDTO roleDTO);
+
+    ResponsePayload<RoleDTO> updateRole(RoleDTO roleDTO);
+
+    ResponsePayload<List<RoleDTO>> findAllRoles();
+
+}
