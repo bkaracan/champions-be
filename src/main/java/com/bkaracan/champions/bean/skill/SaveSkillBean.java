7494dd5a750f929d83adb71919c333efc5fc283b
= <=>
2024-02-08 15:58:17 +0300

    +all end-points completed

--- src/main/java/com/bkaracan/champions/bean/skill/SaveSkillBean.java
+++ src/main/java/com/bkaracan/champions/bean/skill/SaveSkillBean.java
@@ -1,10 +1,12 @@
 package com.bkaracan.champions.bean.skill;
 
 import com.bkaracan.champions.dto.SkillDTO;
+import com.bkaracan.champions.entity.Champion;
 import com.bkaracan.champions.entity.Skill;
 import com.bkaracan.champions.enumeration.core.MessageEnum;
 import com.bkaracan.champions.enumeration.core.ResponseEnum;
 import com.bkaracan.champions.mapper.SkillDtoMapper;
+import com.bkaracan.champions.repository.ChampionRepository;
 import com.bkaracan.champions.repository.SkillRepository;
 import com.bkaracan.champions.responsepayload.AbstractResponsePayload;
 import com.bkaracan.champions.responsepayload.ResponsePayload;
@@ -13,20 +15,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class SaveSkillBean extends AbstractResponsePayload {
 
     private final SkillRepository skillRepository;
+    private final ChampionRepository championRepository;
     private final SkillDtoMapper skillDtoMapper;
 
     @Transactional
     public ResponsePayload<SkillDTO> saveSkill(SkillDTO skillDTO) {
-        if (skillDTO.getId() == null) {
-            Skill savedSkill = skillRepository.save(skillDtoMapper.convertToEntity(skillDTO));
-            SkillDTO savedSkillDTO = skillDtoMapper.map(savedSkill);
-            return setResponse(ResponseEnum.OK, MessageEnum.SAVE_SUCCESS, savedSkillDTO);
+        Optional<Champion> championOptional = championRepository.findById(skillDTO.getChampionId());
+        if (championOptional.isEmpty()) {
+            return setResponse(ResponseEnum.NOT_FOUND, MessageEnum.CHAMPION_NOT_FOUND.getMessage());
         }
-        return setResponse(ResponseEnum.ERROR, MessageEnum.ID_MUST_BE_NULL.getMessage());
+
+        Skill skill = skillDtoMapper.convertToEntity(skillDTO);
+        skill.setChampion(championOptional.get()); // Champion'ı Skill'e ata
+        Skill savedSkill = skillRepository.save(skill);
+
+        return setResponse(ResponseEnum.OK, MessageEnum.SAVE_SUCCESS, skillDtoMapper.map(savedSkill));
     }
 }
