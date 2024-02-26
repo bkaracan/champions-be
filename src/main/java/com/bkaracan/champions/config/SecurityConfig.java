fbbd2a7040b2909a5a85d20f817318caf54e4ed9
= <=>
2024-02-26 15:43:00 +0300

    ++google oAuth2 added

--- src/main/java/com/bkaracan/champions/config/SecurityConfig.java
+++ src/main/java/com/bkaracan/champions/config/SecurityConfig.java
@@ -6,13 +6,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,28 +26,37 @@ public class SecurityConfig {
     private final UserDetailServiceImpl userDetailServiceImpl;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private static final String ROLE_ADMIN = "ADMIN";
+    private static final String ROLE_USER = "ROLE_USER"; // Assuming your users have a basic role
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // Sensitive: csrf protection is entirely disabled
+                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests(authorize -> authorize
-                        .requestMatchers("/login/**", "/register/**").permitAll()
+                        .requestMatchers("/login/**", "/register/**", "/oauth2/**").permitAll() // Allow oauth2 URLs
                         .requestMatchers(AUTH_WHITELIST).permitAll()
-                        .requestMatchers("/api/v1/role/saveRole", "/api/v1/role/updateRole").hasAuthority(ROLE_ADMIN)
-                        .requestMatchers("api/v1/champion/saveChampion", "/api/v1/champion/updateChampion").hasAuthority(ROLE_ADMIN)
-                        .requestMatchers("/api/v1/skill/saveSkill", "/api/v1/skill/updateSkill").hasAuthority(ROLE_ADMIN)
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService()))); // Custom OAuth2UserService
+
         return http.build();
+    }
+
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return request -> {
+            OAuth2User oAuth2User = delegate.loadUser(request);
+            return oAuth2User; // Return the user info
+        };
     }
 
     private static final String[] AUTH_WHITELIST = {
             "/v3/api-docs/**",
-            "v3/api-docs.yaml",
+            "/v3/api-docs.yaml",
             "/swagger-ui/**",
             "/swagger-ui.html"
     };
@@ -53,10 +64,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
     }
 }
