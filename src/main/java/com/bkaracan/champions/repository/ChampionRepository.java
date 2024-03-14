25efb9bde9f5931c1af614c09f9fb617ba2b7b91
= <=>
2024-03-14 14:54:03 +0300

    ++

--- src/main/java/com/bkaracan/champions/repository/ChampionRepository.java
+++ src/main/java/com/bkaracan/champions/repository/ChampionRepository.java
@@ -1,12 +1,16 @@
 package com.bkaracan.champions.repository;
 
 import com.bkaracan.champions.entity.Champion;
+import com.bkaracan.champions.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ChampionRepository extends JpaRepository<Champion, Long>,
         JpaSpecificationExecutor<Champion> {
 
+    Optional<Champion> findByName(String name);
 }
